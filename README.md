1efac3521ccb154fe0fce675a008fe1e281a30db
Tanita <tanitakidane@gmail.com>
2020-04-20 14:53:52 -0400

    Modified gitignore, readme, keys and liri.

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# liri-node-app
+# LIRI-Bot
+
+Description
+LIRI-bot (Language Interpretation and Recognition Interface) is a CLI (Command Line Interface) app built through the use of node.js which, not unlike it's SIRI namesake, accepts user input and facilitates an appropriate response. In the case of LIRI user input comes in the form of command line arguments which, that this context, are further defined as either a "command" or a "command argument". LIRI accepts these commands and responds with the appropriate predefined function.
+
+Installation
+In order to run LIRI-bot simply fork this repository and within the cloned directory install it's node dependencies with:
+
+npm install
+Functionality
+LIRI currently accepts the following "commands":
+
+concert-this
+node liri.js 'concert-this' 'artist name'
+The 'concert-this' command will return the next four upcoming events/concerts for a given artist (if there are any). This information is obtained by intefacing with the bandsintown API.
+
+spotify-this
+node liri.js 'spotify-this' 'song name or search term'
+The 'spotify-this' command returns the Spotify information and link for a given song or search term.
+
+movie-this
+node liri.js 'movie-this' 'movie name'
+The 'movie-this' command will accept a movie name and search for the corresponding movie's information through the omdb API.
+
+do-what-it-says
+node liri.js 'do-what-it-says'
+The 'do-what-it-says' command will run the first command that is logged in the random.txt file.
---
+++ liri.js
@@ -0,0 +1,154 @@
+// Code to read set any enviroment variables with the dotenv package
+require('dotenv').config();
+
+// Importing files needed to run the functions
+var fs = require('fs');
+var request = require('request');
+var keys =  require('./keys.js');
+var Spotify = require('node-spotify-api');
+var moment = require('moment');
+
+var spotify = new Spotify({
+    id: keys.spotify.id,
+    secret: keys.spotify.secret
+});
+
+var inputCommand = process.argv[2];
+var inputParam = process.argv.slice(3).join(' ');
+
+function userRequest(command, param) {
+
+    switch(command) {
+        case 'spotify-this-song':
+        spotifyThis(param);
+        break;
+        
+        case 'concert-this':
+        concertThis(param);
+        break;
+
+        case 'movie-this':
+        movieThis(param);
+        break;
+
+        case 'do-what-it-says':
+        getRandom();
+        break;
+
+        // If no command is entered, this is the default message to user
+        default:
+        console.log('Please enter a valid command: spotify-this-song, concert-this, movie-this, or do-what-it-says.');
+    }
+    
+}
+function spotifyThis(song) {
+    if (!song) {
+        song = 'The Sign by Ace of Base';
+    }
+    spotify.search ({ type: 'track', query: song}, function (err, data){
+        if (err){
+            return console.log('Error occured: ' + err);
+            
+        }
+        else{
+            var tracks = data.tracks.items;
+            for (var i =  0; i < tracks.length; i++) {
+                console.log("------------------------------");
+                var artistArr = tracks[i].artists;
+                var artists = [];
+                for (var j = 0; j < artistArr.length; j++){
+                    artists.push(artistArr[j].name);
+                    
+                }
+               console.log('Artist: ' + artists.join(", "));
+                console.log('Song: ' + tracks[i].name);
+                console.log('Album: ' + tracks[i].album.name);
+
+                if (tracks[i].preview_url == null) {
+                    console.log ('Preview unavailable');
+                }
+                else{
+                    console.log('Preview: ' + tracks[i].preview_url);
+                }
+                console.log("------------------------------")
+            }
+                
+        }
+    });
+}
+
+function concertThis(artist) {
+    var artistQ = artist.split(' ').join('+');
+
+    var queryURL = "https://rest.bandsintown.com/artists/" + artistQ + "/events?app_id=" + keys.bandsintown;
+    request(queryURL, function(err, response, body) {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        else{
+            var result = JSON.parse(body);
+            console.log(result);
+            for (var i = 0; i < result.length; i++) {
+                console.log("------------------------------")
+                console.log("Venue: " + result[i].venue.name);
+                console.log("Location: " + result[i].venue.city + ", " + (result[i].venue.region || result[i].venue.country));
+                console.log("Date: " + moment(result[i].datetime).format("MM/DD/YYYY"));
+                console.log("------------------------------")
+            }
+        }
+    });
+}
+
+function movieThis(movie){
+    if(!movie) {
+       movie = "Mr. Nobody";
+    }
+
+    let queryURL = 'http://www.omdbapi.com/?t=' + movie +'&y=&plot=long&tomatoes=true&r=json&apikey=trilogy'
+
+    let movieQ = movie.split(' ').join('+');
+
+    request(queryURL, function(error, response, body) {
+
+        if(error) {
+            console.log(error);
+        } else {
+            let result = JSON.parse(body);
+            console.log("------------------------------")
+            console.log("Title: " + result["Title"]);
+	        console.log("Year: " + result["Year"]);
+            console.log("IMDB Rating: " + result["imdbRating"]);
+            if ( result.Ratings[1]) {
+                console.log("Rotten Tomatoes Rating: " + result.Ratings[1].Value);
+            }
+            
+	        console.log("Country: " + result["Country"]);
+	        console.log("Language: " + result["Language"]);
+	        console.log("Plot: " + result["Plot"]);
+	        console.log("Actors: " + result["Actors"]);
+            console.log("Rotten Tomatoes URL: " + result["tomatoURL"])
+            console.log("------------------------------")
+    }
+        });
+}
+
+
+function getRandom() {
+    fs.readFile("random.txt", "utf8", function(error, data) {
+        if(error){
+            console.log(error);
+        }else {
+            var dataSplit = data.split(',');
+            var readCommand = dataSplit[0];
+            var readParam = dataSplit[1];
+            
+        
+
+            userRequest(readCommand, readParam);
+        }
+    })
+}
+
+
+ userRequest(inputCommand, inputParam);
